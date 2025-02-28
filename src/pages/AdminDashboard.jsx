437f1055d1e177746d6687e82b200a519fa629e4
Anisha2123 <birlaani@gmail.com>
2025-03-01 05:19:58 +0530

    Fix Vite permission issue

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -12,6 +12,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
 
+
 import pdfWorker from "/pdf.worker.min.js?url";
 
 pdfjs.GlobalWorkerOptions.workerSrc = pdfWorker;
